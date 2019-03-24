c8feccccb08b1669c6c194335f29db468796aa0c
ashwin <ashwinkn@hotmail.com>
2019-03-24 14:17:41 -0500

    Updated snapshots

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -14,6 +14,7 @@ describe('# integration test', () => {
         let output = execSync(
             'sgen -g pom -g `pwd`/dist/reactor.min.js -d src/test/fixture/design.json -o testoutput'
         ).toString();
+        output = output.replace(/info: Loaded generator .*reactor.min.js.*/, '');
         expect(output).toMatchSnapshot();
         execSync('npm install', { cwd: 'testoutput', stdio: 'inherit' });
         execSync('npm run lint', { cwd: 'testoutput', stdio: 'inherit' });

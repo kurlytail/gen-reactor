a9d02242daae6361544d69e2764eda2670cca6a0
ashwin <ashwinkn@hotmail.com>
2019-03-24 13:42:40 -0500

    Added templates

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -7,17 +7,16 @@ describe('# integration test', () => {
 
     it('## should print help options', () => {
         let output = execSync('npm run build').toString();
-        output = execSync('sgen -g `pwd`/dist/reactor.min.js -h').toString();
+        output = execSync('sgen -g `pwd`/dist/pom.min.js -h').toString();
         expect(output).toMatchSnapshot();
     });
-
-    it('## should generate design and run reactor commands', () => {
-        let output = execSync('npm run build').toString();
-        output = execSync('sgen -g `pwd`/dist/reactor.min.js -d src/test/fixture/design.json -o testoutput').toString();
-        output = output.replace(/info: Loaded generator .*reactor.min.js.*/, '');
+    it('## should generate design and run pom commands with reactor extensions', () => {
+        let output = execSync(
+            'sgen -g pom -g `pwd`/dist/reactor.min.js -d src/test/fixture/design.json -o testoutput'
+        ).toString();
         expect(output).toMatchSnapshot();
         output = execSync('npm install', { cwd: 'testoutput' }).toString();
         output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
-        output = execSync('npm run build', { cwd: 'testoutput' }).toString();
+        output = execSync('mvn compile', { cwd: 'testoutput' }).toString();
     });
 });
--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ basePath = Path.relative(process.cwd(), basePath) || '.';
 const options = {
     // eslint-disable-next-line no-undef
     version: app.version,
-    map: [`${basePath}/templates/map.json`]
+    extension: [`${basePath}/templates/extensions`]
 };
 
 export default options;

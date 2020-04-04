d66116d42fc29ffe9b2e481297a1c12f6474544a
Ashwin Kamath <ashwinkn@hotmail.com>
2020-04-04 18:20:14 -0500

    generated by     - gen-lib@1.3.130     - @kurlytail/gen-lib@1.3.130     - @kurlytail/gen-sgen@1.3.105     - @kurlytail/gen-log@1.3.144     - @kurlytail/gen-bst@1.3.122

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -10,11 +10,15 @@ describe('# integration test', () => {
         output = execSync('sgen -g `pwd`/dist/pom.min.js -h').toString();
         expect(output).toMatchSnapshot();
     });
+    
     it('## should generate design and run pom commands with reactor extensions', () => {
         let output = execSync(
             'sgen -g pom -g `pwd`/dist/reactor.min.js -d src/test/fixture/design.json -o testoutput'
         ).toString();
         output = output.replace(/info: Loaded generator .*reactor.min.js.*/, '');
+        output = output
+            .replace(/warn: Please cherrypick changes from master-sgen-generated from .*/, '')
+            .replace(/info: git cherry-pick .*/, '');
         expect(output).toMatchSnapshot();
         execSync('npm install', { cwd: 'testoutput', stdio: 'inherit' });
         execSync('npm run lint', { cwd: 'testoutput', stdio: 'inherit' });
